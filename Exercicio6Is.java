18f920f4e139bf7a30648f8cb13443679e143ec6
Natan Farage Cohen <natan.cohen@sga.pucminas.br>
2025-02-19 19:36:19 -0300

    inicioEx06

---
+++ Exercicio6Is.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Exercicio6Is {
+static Scanner entrada = new Scanner(System.in);
+
+    public static boolean fraseVogal(String fraseV){
+        boolean resp = false;
+        char[] ehVogal = 
+        for(int i = 0; i < fraseV.length(); i++){
+            
+        }
+
+        return resp;
+    }
+    
+    
+    public static void main(String[] args) {
+        
+    }
+}
--- MinhaClasse.java
+++ MinhaClasse.java
@@ -1,4 +1,3 @@
-package Codigos2025;
 import java.util.Scanner;
 
 public class MinhaClasse {
